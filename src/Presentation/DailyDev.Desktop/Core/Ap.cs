d52f103cd455974b41b7e66c769ac1c615083346
musmanrafiq <usman.solemn@gmail.com>
2020-10-28 23:34:17 +0500

    MVVMCross library integrated

---
+++ src/Presentation/DailyDev.Desktop/Core/Ap.cs
@@ -0,0 +1,13 @@
+﻿using DailyDev.Desktop.ViewModels;
+using MvvmCross.ViewModels;
+
+namespace DailyDev.Desktop.Core
+{
+    public class Ap : MvxApplication
+    {
+        public override void Initialize()
+        {
+            RegisterAppStart<SiteViewModel>();
+        }
+    }
+}
