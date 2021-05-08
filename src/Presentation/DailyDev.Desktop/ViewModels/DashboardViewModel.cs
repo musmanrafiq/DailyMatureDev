5ca992ce461d22ed7168bdd53e5e88ee8c3a2e24
usman_solemn <usman.solemn@gmail.com>
2021-05-08 14:06:39 +0500

    implementation

--- src/Presentation/DailyDev.Desktop/ViewModels/DashboardViewModel.cs
+++ src/Presentation/DailyDev.Desktop/ViewModels/DashboardViewModel.cs
@@ -1,9 +1,7 @@
-﻿using DailyDev.Desktop.Helpers;
-using DailyDev.Desktop.Views;
+﻿using DailyDev.Desktop.Views;
 using DailyDev.Domain.Business.Models;
 using DailyDev.Domain.Data;
 using DailyDev.Domain.Models;
-using DailyDev.Infrastructure.Communication.Services.Smtp;
 using DailyDev.Infrastructure.Services;
 using MvvmCross.Commands;
 using MvvmCross.ViewModels;
@@ -74,7 +72,7 @@ namespace DailyDev.Desktop.ViewModels
             PostView p = new PostView();
             p.ShowDialog();
 
-            using var dbContext = new DailyDevDbContext();
+            /*using var dbContext = new DailyDevDbContext();
             var links = dbContext.TempLinks.ToList();
             if (links.Any())
             {
@@ -89,252 +87,252 @@ namespace DailyDev.Desktop.ViewModels
                 var smtpService = new SmtpService();
                 var prepareTitle = await PrepareTitleForMyBlogPostAsync();
                 smtpService.SendEmail(prepareTitle, htmlBody);
-            }
-        }
+        }*/
+    }
 
-        private async Task<string> PrepareTitleForMyBlogPostAsync()
+    private async Task<string> PrepareTitleForMyBlogPostAsync()
+    {
+
+        FeedService service = new FeedService();
+        var (feedItemModel, error) = await service.FetchLatestPost("https://www.usmanrafiq.com/feeds/posts/default");
+        if (feedItemModel != null)
         {
+            var postNumber = int.Parse(feedItemModel.Title.Split('#')[1]);
+            return $"The Morning Mash # {postNumber + 1}";
+        }
+        return $"The Morning Mash # {DateTime.Now.ToString("ddmmyyyy")}";
+    }
+
+    private void OnBlogSelectAsync(FeedItemModel item)
+    {
+
+        var psi = new ProcessStartInfo
+        {
+            FileName = item.Link,
+            UseShellExecute = true
+        };
+        Process.Start(psi);
+    }
+
+    private async Task OnRemoveSiteAsync(SiteModel model)
+    {
+        using var dbContext = new DailyDevDbContext();
+        dbContext.Remove(model);
+        await dbContext.SaveChangesAsync();
+        Sites.Remove(model);
+        TotalBlogsCount = Sites.Count();
+    }
+
+    public DashboardViewModel()
+    {
+        //initialize 
+        _postsToPublish = new List<FeedItemModel>();
+
+        ButtonText = "Add Site";
+        AddSiteCommand = new MvxCommand(AddSite);
+
+        using var dbContext = new DailyDevDbContext();
+        var sites = dbContext.SiteModels.ToList();
+        foreach (SiteModel site in sites)
+        {
+            Sites.Add(site);
+        }
+        TotalBlogsCount = Sites.Count();
+    }
+
+    public async Task OnItemSelectedAsync(SiteModel model)
+    {
+        if (model != null)
+        {
+            BlogPosts.Clear();
+            ButtonText = "Update Site";
+            Url = model.Url;
+            Name = model.Name;
+            Id = model.Id;
 
             FeedService service = new FeedService();
-            var (feedItemModel, error) = await service.FetchLatestPost("https://www.usmanrafiq.com/feeds/posts/default");
-            if (feedItemModel != null)
+            var (feedModel, error) = await service.FetchAsync(model.Url);
+            if (feedModel != null)
             {
-                var postNumber = int.Parse(feedItemModel.Title.Split('#')[1]);
-                return $"The Morning Mash # {postNumber + 1}";
-            }
-            return $"The Morning Mash # {DateTime.Now.ToString("ddmmyyyy")}";
-        }
-
-        private void OnBlogSelectAsync(FeedItemModel item)
-        {
-
-            var psi = new ProcessStartInfo
-            {
-                FileName = item.Link,
-                UseShellExecute = true
-            };
-            Process.Start(psi);
-        }
-
-        private async Task OnRemoveSiteAsync(SiteModel model)
-        {
-            using var dbContext = new DailyDevDbContext();
-            dbContext.Remove(model);
-            await dbContext.SaveChangesAsync();
-            Sites.Remove(model);
-            TotalBlogsCount = Sites.Count();
-        }
-
-        public DashboardViewModel()
-        {
-            //initialize 
-            _postsToPublish = new List<FeedItemModel>();
-
-            ButtonText = "Add Site";
-            AddSiteCommand = new MvxCommand(AddSite);
-
-            using var dbContext = new DailyDevDbContext();
-            var sites = dbContext.SiteModels.ToList();
-            foreach (SiteModel site in sites)
-            {
-                Sites.Add(site);
-            }
-            TotalBlogsCount = Sites.Count();
-        }
-
-        public async Task OnItemSelectedAsync(SiteModel model)
-        {
-            if (model != null)
-            {
+                BlogTitle = feedModel.Title;
                 BlogPosts.Clear();
-                ButtonText = "Update Site";
-                Url = model.Url;
-                Name = model.Name;
-                Id = model.Id;
-
-                FeedService service = new FeedService();
-                var (feedModel, error) = await service.FetchAsync(model.Url);
-                if (feedModel != null)
+                foreach (FeedItemModel item in feedModel.Items)
                 {
-                    BlogTitle = feedModel.Title;
-                    BlogPosts.Clear();
-                    foreach (FeedItemModel item in feedModel.Items)
-                    {
-                        BlogPosts.Add(item);
-                    }
+                    BlogPosts.Add(item);
                 }
-                else
-                {
-                    var psi = new ProcessStartInfo
-                    {
-                        FileName = model.Url,
-                        UseShellExecute = true
-                    };
-                    Process.Start(psi);
-
-                    NotificationBackground = Brushes.Red;
-                    NotificationText = error;
-                    ShowNotification = Visibility.Visible;
-
-
-
-                    await Task.Factory.StartNew(
-    async () =>
-    {
-        await Task.Delay(2000);
-
-        NotificationBackground = Brushes.Green;
-        NotificationText = string.Empty;
-        ShowNotification = Visibility.Collapsed;
-    }
-);
-
-                }
-            }
-        }
-
-
-        public IMvxCommand AddSiteCommand { get; set; }
-        public IMvxCommand ItemClickedCommand { get; set; }
-        public bool CanAddSite => Url?.Length > 0 && Name?.Length > 0;
-        public int TotalBlogsCount { get; set; } = 0;
-        public void AddSite()
-        {
-
-            SiteModel site = new SiteModel
-            {
-                Id = Id,
-                Url = Url,
-                Name = Name
-            };
-            //Url = string.Empty;
-
-            using var dbContext = new DailyDevDbContext();
-            if (Id > 0)
-            {
-                dbContext.Update(site);
             }
             else
             {
-                dbContext.SiteModels.Add(site);
+                var psi = new ProcessStartInfo
+                {
+                    FileName = model.Url,
+                    UseShellExecute = true
+                };
+                Process.Start(psi);
+
+                NotificationBackground = Brushes.Red;
+                NotificationText = error;
+                ShowNotification = Visibility.Visible;
+
+
+
+                await Task.Factory.StartNew(
+async () =>
+{
+    await Task.Delay(2000);
+
+    NotificationBackground = Brushes.Green;
+    NotificationText = string.Empty;
+    ShowNotification = Visibility.Collapsed;
+}
+);
 
             }
-            dbContext.SaveChanges();
-
-            if (Id > 0)
-            {
-                var existingRecord = Sites.FirstOrDefault(x => x.Id == Id);
-                Sites.Remove(existingRecord);
-            }
-
-            Sites.Add(site);
-
-            Id = 0;
-            Url = string.Empty;
-            Name = string.Empty;
-            ButtonText = "Add Site";
-
-            TotalBlogsCount = Sites.Count();
-        }
-        private ObservableCollection<Domain.Models.SiteModel> _sites = new ObservableCollection<Domain.Models.SiteModel>();
-
-        public ObservableCollection<Domain.Models.SiteModel> Sites
-        {
-            get { return _sites; }
-            set { SetProperty(ref _sites, value); }
-        }
-
-        private int _id;
-        public int Id
-        {
-            get { return _id; }
-            set
-            {
-                SetProperty(ref _id, value);
-            }
-        }
-
-        private string _buttonText;
-
-        public string ButtonText
-        {
-            get { return _buttonText; }
-            set
-            {
-                SetProperty(ref _buttonText, value);
-            }
-        }
-
-        private string _name;
-
-        public string Name
-        {
-            get { return _name; }
-            set
-            {
-                SetProperty(ref _name, value);
-                RaisePropertyChanged(() => CanAddSite);
-            }
-        }
-        private string _url;
-
-        public string Url
-        {
-            get { return _url; }
-            set
-            {
-                SetProperty(ref _url, value);
-                RaisePropertyChanged(() => CanAddSite);
-            }
-        }
-
-        private string _blogTitle;
-
-        public string BlogTitle
-        {
-            get { return _blogTitle; }
-            set
-            {
-                SetProperty(ref _blogTitle, value);
-            }
-        }
-
-        private Visibility _showNotification;
-        public Visibility ShowNotification
-        {
-            get { return _showNotification; }
-            set
-            {
-                SetProperty(ref _showNotification, value);
-            }
-        }
-
-        private Brush _notificationBackground;
-        public Brush NotificationBackground
-        {
-            get { return _notificationBackground; }
-            set
-            {
-                SetProperty(ref _notificationBackground, value);
-            }
-        }
-
-        private string _notificationText;
-        public string NotificationText
-        {
-            get { return _notificationText; }
-            set
-            {
-                SetProperty(ref _notificationText, value);
-            }
-        }
-
-        private ObservableCollection<FeedItemModel> _blogPosts = new ObservableCollection<FeedItemModel>();
-
-        public ObservableCollection<FeedItemModel> BlogPosts
-        {
-            get { return _blogPosts; }
-            set { SetProperty(ref _blogPosts, value); }
         }
     }
+
+
+    public IMvxCommand AddSiteCommand { get; set; }
+    public IMvxCommand ItemClickedCommand { get; set; }
+    public bool CanAddSite => Url?.Length > 0 && Name?.Length > 0;
+    public int TotalBlogsCount { get; set; } = 0;
+    public void AddSite()
+    {
+
+        SiteModel site = new SiteModel
+        {
+            Id = Id,
+            Url = Url,
+            Name = Name
+        };
+        //Url = string.Empty;
+
+        using var dbContext = new DailyDevDbContext();
+        if (Id > 0)
+        {
+            dbContext.Update(site);
+        }
+        else
+        {
+            dbContext.SiteModels.Add(site);
+
+        }
+        dbContext.SaveChanges();
+
+        if (Id > 0)
+        {
+            var existingRecord = Sites.FirstOrDefault(x => x.Id == Id);
+            Sites.Remove(existingRecord);
+        }
+
+        Sites.Add(site);
+
+        Id = 0;
+        Url = string.Empty;
+        Name = string.Empty;
+        ButtonText = "Add Site";
+
+        TotalBlogsCount = Sites.Count();
+    }
+    private ObservableCollection<Domain.Models.SiteModel> _sites = new ObservableCollection<Domain.Models.SiteModel>();
+
+    public ObservableCollection<Domain.Models.SiteModel> Sites
+    {
+        get { return _sites; }
+        set { SetProperty(ref _sites, value); }
+    }
+
+    private int _id;
+    public int Id
+    {
+        get { return _id; }
+        set
+        {
+            SetProperty(ref _id, value);
+        }
+    }
+
+    private string _buttonText;
+
+    public string ButtonText
+    {
+        get { return _buttonText; }
+        set
+        {
+            SetProperty(ref _buttonText, value);
+        }
+    }
+
+    private string _name;
+
+    public string Name
+    {
+        get { return _name; }
+        set
+        {
+            SetProperty(ref _name, value);
+            RaisePropertyChanged(() => CanAddSite);
+        }
+    }
+    private string _url;
+
+    public string Url
+    {
+        get { return _url; }
+        set
+        {
+            SetProperty(ref _url, value);
+            RaisePropertyChanged(() => CanAddSite);
+        }
+    }
+
+    private string _blogTitle;
+
+    public string BlogTitle
+    {
+        get { return _blogTitle; }
+        set
+        {
+            SetProperty(ref _blogTitle, value);
+        }
+    }
+
+    private Visibility _showNotification;
+    public Visibility ShowNotification
+    {
+        get { return _showNotification; }
+        set
+        {
+            SetProperty(ref _showNotification, value);
+        }
+    }
+
+    private Brush _notificationBackground;
+    public Brush NotificationBackground
+    {
+        get { return _notificationBackground; }
+        set
+        {
+            SetProperty(ref _notificationBackground, value);
+        }
+    }
+
+    private string _notificationText;
+    public string NotificationText
+    {
+        get { return _notificationText; }
+        set
+        {
+            SetProperty(ref _notificationText, value);
+        }
+    }
+
+    private ObservableCollection<FeedItemModel> _blogPosts = new ObservableCollection<FeedItemModel>();
+
+    public ObservableCollection<FeedItemModel> BlogPosts
+    {
+        get { return _blogPosts; }
+        set { SetProperty(ref _blogPosts, value); }
+    }
+}
 }
